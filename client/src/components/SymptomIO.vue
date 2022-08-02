31f077b9a6445004e41c4f892907b09dabff7ab7
Cobus von Wielligh <cobus@provar.co.za>
2022-08-02 09:37:42 +0200

    feat: responsive text-highligting, general styling

--- client/src/components/SymptomIO.vue
+++ client/src/components/SymptomIO.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
+
+import Highlighter from 'vue-highlight-words'
+
 defineProps < {
     symptoms: string;
     title: string;
+    keywords: string[];
 } > ();
+
 </script>
 
 <template>
 <div class="wrapper">
-    <div class="input-text">
+    <div class="input-text-field">
         <input v-model="symptoms" class="search" placeholder="Enter text here..." />
     </div>
-    <h3 class="output-text">{{ symptoms }}</h3>
+    <!-- <h3 class="output-text">{{ symptoms }}</h3> -->
+    <Highlighter class="output-text" :style="{ color: 'white' }"
+      highlightClassName="highlight"
+      :searchWords="keywords"
+      :autoEscape="true"
+      :textToHighlight="symptoms"/>
+    
 </div>
 </template>
 
@@ -32,26 +43,32 @@ h3 {
 
 .output-text {
     text-align: center;
-    word-wrap: break-word;
-    width: 9rem;
+    word-wrap: normal;
+    width: 9rem !important;
+    height: 2rem;
 }
 
 .search {
     display: inline-block;
     font-weight: 600;
     color: #429af8;
-    text-align: center;
+    text-align: left;
     vertical-align: middle;
     background-color: #fafafae9;
     border: 0.0625rem solid transparent;
-    padding: 0.75rem 1rem;
+    padding: 1rem 1rem;
     padding-left: 1rem;
     padding-top: 1rem;
     font-size: 1rem;
     line-height: 3;
     border-radius: 0.3125rem;
-    width: 10rem;
+    width: 15rem;
+    height: 2rem;
     text-align: center;
+}
+
+.input-text-field {
+    padding-top: 0.25rem;
 }
 
 .title h1,
